b813414ad4f054ee5310b1b71c083d8d1feba0ed
Stas Savin <asavin@ukr.net>
2016-12-04 16:59:45 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <title>Git test</title>
+    </head>
+    <body>
+    <h1>Hello world!</h1>
+    <div style="height: 100px; weight: 200px; background: darkred;">
+        <p>lalala</p>
+    </div>
+    </body>
+</html>
